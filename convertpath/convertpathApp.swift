0ad2d51d0182cca67a2bd84b84e096b540b6bcd1
N-I-K123 <k.zelazekk@gmail.com>
2024-04-17 10:21:12 +0200

    Initial Commit

---
+++ convertpath/convertpathApp.swift
@@ -0,0 +1,17 @@
+//
+//  convertpathApp.swift
+//  convertpath
+//
+//  Created by Krzysztof Żelazek on 17/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct convertpathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
