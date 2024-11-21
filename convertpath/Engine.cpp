5d02770c62018972803c02c783c978161f7b7208
N-I-K123 <k.zelazekk@gmail.com>
2024-11-21 11:58:51 +0100

    first commit

---
+++ convertpath/Engine.cpp
@@ -0,0 +1,8 @@
+//
+//  Engine.cpp
+//  convertpath
+//
+//  Created by Krzysztof Żelazek on 17/04/2024.
+//
+
+#include "Engine.hpp"
---
+++ convertpath/Engine.h
@@ -0,0 +1,13 @@
+//
+//  Engine.hpp
+//  convertpath
+//
+//  Created by Krzysztof Żelazek on 17/04/2024.
+//
+
+#ifndef Engine_hpp
+#define Engine_hpp
+
+#include <stdio.h>
+
+#endif /* Engine_hpp */
