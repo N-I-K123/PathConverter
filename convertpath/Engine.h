9c20c1842d0801e3da824332e8a1e4d87242126a
N-I-K123 <116213882+N-I-K123@users.noreply.github.com>
2024-11-21 12:02:20 +0100

    Update Engine.h

--- convertpath/Engine.h
+++ convertpath/Engine.h
@@ -5,9 +5,36 @@
 //  Created by Krzysztof Żelazek on 17/04/2024.
 //
 
-#ifndef Engine_hpp
-#define Engine_hpp
+#ifndef Engine_h
+#define Engine_h
 
 #include <stdio.h>
+#include <string>
 
 #endif /* Engine_hpp */
+
+#ifdef __cplusplus
+extern "C++" {
+#endif
+
+    void engine(std::string path);
+    std::string macToWin(std::string path);
+    std::string remote(std::string path);
+    std::string winToMac(std::string path);
+    std::string getConvertedPath();
+    void prefixMainMac();
+    void setPrefix(std::string prefix);
+    void setPrefixStatus(bool status);
+    int findSystem(std::string path);
+    void setConversionDestination(bool dst);
+
+    std::string prefixFinal;
+    bool prefixStatus;
+    std::string convertedPath;
+    bool conversionDestination;
+    std::string pf;
+    
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
