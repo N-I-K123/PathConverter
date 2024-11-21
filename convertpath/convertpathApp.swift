f1ea1d1c194768199e80155e0044c7829a2da7d7
N-I-K123 <116213882+N-I-K123@users.noreply.github.com>
2024-11-21 12:03:40 +0100

    Update convertpathApp.swift

--- convertpath/convertpathApp.swift
+++ convertpath/convertpathApp.swift
@@ -9,9 +9,55 @@ import SwiftUI
 
 @main
 struct convertpathApp: App {
+    @AppStorage("viewStatus") var isSimplifiedUI: Bool = false
+       
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .commands {
+                    CommandMenu("Widok") {
+                        Toggle("Uproszczony UI", isOn: $isSimplifiedUI)
+                            .keyboardShortcut("s", modifiers: [.command])
+                }
+        }
+       
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+        @AppStorage("viewStatus") var isSimplifiedUI: Bool = false
+
+        func applicationDidFinishLaunching(_ notification: Notification) {
+            if let window = NSApplication.shared.windows.first {
+                self.window = window
+                updateWindowSize()  // Ustawiamy rozmiar okna przy starcie aplikacji
+            }
+        }
+        
+        func updateWindowSize() {
+            let simplifiedSize = NSSize(width: 100, height: 100)
+            let fullSize = NSSize(width: 900, height: 400)
+
+            if isSimplifiedUI {
+                setWindowSize(simplifiedSize)
+            } else {
+                setWindowSize(fullSize)
+            }
+        }
+        
+        func setWindowSize(_ size: NSSize) {
+            // Ustawienie rozmiaru okna na podany rozmiar
+            var frame = window.frame
+            frame.size = size
+            window.setFrame(frame, display: true, animate: true)
+            
+            // Blokowanie zmiany rozmiaru przez użytkownika
+            window.minSize = size
+            window.maxSize = size            
+        }
+
 }
